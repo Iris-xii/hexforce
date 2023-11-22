1417f2427f41989460d6162cbce8690623518395
Iris <48930896+Iris-xii@users.noreply.github.com>
2023-11-22 19:40:30 +0100

    Add files via upload

---
+++ autohexforce.lua
@@ -0,0 +1,55 @@
+
+function run(s)
+    local filename = "https://raw.githubusercontent.com/Iris-xii/hexforce/main/Rest/" .. s .. ".txt"
+    local t = {filename}
+    
+    
+    if s == "Craft_Phial" then
+        t = {}
+        for i=1,45 do
+            t[#t+1] = "https://raw.githubusercontent.com/Iris-xii/hexforce/main/Craft_Phial/Craft_Phial_"..i..".txt"
+        end
+    elseif s == "Flay_Mind" then
+        t = {}
+        for i=1,2 do
+            t[#t+1] = "https://raw.githubusercontent.com/Iris-xii/hexforce/main/Flay_Mind/Flay_Mind_"..i..".txt"
+        end
+    elseif s == "Consume_Wisp" then
+        t = {}
+        for i=1,5 do
+            t[#t+1] = "https://raw.githubusercontent.com/Iris-xii/hexforce/main/Consume_Wisp/Consume_Wisp_"..i..".txt"
+        end
+    end
+    
+    print("Forcing pattern: "..s)
+    shell.run("hexforce", table.unpack(t))
+end
+
+local spells = {
+    --"Dispel_Rain",
+    "Accelerate",
+    "Consume_Wisp",
+    "Craft_Phial",
+    "Flay_Mind",
+    --"Bind_Figment",
+    "Bind_Wisp",
+    "Black_Suns_Zenith",
+    "Blue_Suns_Zenith",
+    "Create_Lava",
+    "Flight",
+    "Gates_Reflection",
+    "Greater_Teleport",
+    "Green_Suns_Zenith",
+    "Propulsion",
+    "Red_Suns_Zenith",
+    "Summon_Greater_Sentinel",
+    "Summon_Lightning",
+    "Summon_Rain",
+    --"Suspicious_Glyph", better not to
+    "Twokais_Ideal_Condition",
+    "White_Suns_Zenith",
+}
+for k,v in pairs(spells) do
+    run(v)
+    os.sleep(4)
+end
